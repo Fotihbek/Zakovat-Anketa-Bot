455f6e04e915f013418e8b38d3f13c2dc9b5ad6a
Fotihbek Komilov <95439454+Fotihbek@users.noreply.github.com>
2022-06-06 10:04:56 +0500

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,16 +1,21 @@
 from aiogram import types
-
+from aiogram.dispatcher import FSMContext
 from loader import dp
-
-@dp.message_handler(text="👨‍💻 Dasturchi")
-async def bot_echo(message: types.Message):
-    await message.answer("Manfaatli taklif!\nHar xil telegram bot va veb-saytlar tuzib berish xizmatini tavsiya etamiz!\n👨‍💻 Dasturchi: @ProgerUzb (Fotihbek Komilov)")
-
-@dp.message_handler(text="📄 Yo'riqnoma")
-async def bot_echo(message: types.Message):
-    await message.answer("https://telegra.ph/Zakovat-uchun-ro%CA%BByxatdan-o%CA%BBtish-01-29")
+from states.Sts import MySt
 
 # Echo bot
 @dp.message_handler(state=None)
+async def bot_echo(message: types.Message, state: FSMContext):
+    await message.answer("Matnning ikkinchi qismini kiriting:")
+    await state.update_data({"id": "", "text1": message.text})
+    await MySt.a.set()
+
+@dp.message_handler(state=MySt.a)
 async def bot_echo(message: types.Message):
-    await message.answer(message.text)
+
+    await state.update_data({"text2": message.text})
+    data = await state.get_data()
+    matn = json.dumps(data)
+    await message.answer(matn)
+    await message.answer(data)
+    await reset_state(with_data=True)
